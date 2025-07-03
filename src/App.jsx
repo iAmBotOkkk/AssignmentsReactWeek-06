ad77aa18dfa22ddb3818dbdb4640578d7492e950
iAmBotOkkk <barmanbishal1234@gmail.com>
2025-07-03 23:20:44 +0530

    Solved assignment week--6

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { Assignment3 } from './components/Assignment3'
 function App() {
   return (
     <div>
-      <Assignment1></Assignment1>
-      {/* <Assignment2 /> */}
+      {/* <Assignment1></Assignment1> */}
+      <Assignment2 />
       {/* <Assignment3 /> */}
     </div>
   )
